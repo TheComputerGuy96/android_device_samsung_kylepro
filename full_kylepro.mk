febda22e0e24d767cc0634b952b7d8ea455590ed
DodoGTA GT <abuttonbits@gmail.com>
2016-05-16 14:55:55 +0300

    Update for AOSP (part 2)

--- full_kylepro.mk
+++ full_kylepro.mk
@@ -1,12 +1,15 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Get the long list of APNs
+PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, build/target/product/aosp_base_telephony.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/kylepro/device_kylepro.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := kylepro
-PRODUCT_NAME := cm_kylepro
+PRODUCT_NAME := full_kylepro
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-S7580
