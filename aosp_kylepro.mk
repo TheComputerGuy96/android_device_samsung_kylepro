63965a0a00d4d5ef94b927d6c91805cad248ba4f
DodoGTA GT <abuttonbits@gmail.com>
2016-05-19 16:04:43 +0300

    Fix derp

--- aosp_kylepro.mk
+++ aosp_kylepro.mk
@@ -1,7 +1,9 @@
 # Inherit device configuration
-$(call inherit-product, device/samsung/kylepro/full_kylepro.mk) 
+$(call inherit-product, device/samsung/kylepro/full_kylepro.mk)
+
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_kylepro 
+PRODUCT_NAME := aosp_kylepro
+
 # Set build fingerprint / ID / Product Name etc.
 PRODUCT_BUILD_PROP_OVERRIDES += \
             PRODUCT_NAME=kyleproxx \
