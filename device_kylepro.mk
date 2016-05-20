b81f937ae27fad2af91505b375d5f480861cadab
DodoGTA GT <abuttonbits@gmail.com>
2016-05-20 06:46:28 +0300

    Add multiple threads for dex2oat

--- device_kylepro.mk
+++ device_kylepro.mk
@@ -139,6 +139,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-flags=--no-watch-dog
 
+# Multiple threads for dex2oat
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.fw.dex2oat_thread_count=2
+
 # Use Awesomeplayer
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.media.use-awesome=true
