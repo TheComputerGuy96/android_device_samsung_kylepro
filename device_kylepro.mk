fe70db8f68869d1d28e47e60e237218f97812f41
DodoGTA GT <abuttonbits@gmail.com>
2016-05-24 06:41:21 +0300

    Workaround compile error

--- device_kylepro.mk
+++ device_kylepro.mk
@@ -157,5 +157,3 @@ PRODUCT_PACKAGES += \
             Stk
 
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
-
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
