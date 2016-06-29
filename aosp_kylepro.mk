aa5434aaf2b27a127217222b8e5dc45db7743a82
DodoGTA GT <vwestlife20@gmail.com>
2016-06-29 13:59:45 +0300

    Various improvements

--- aosp_kylepro.mk
+++ aosp_kylepro.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/samsung/kylepro/full_kylepro.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := kylepro
+PRODUCT_NAME := aosp_kylepro
 
 # Set build fingerprint / ID / Product Name etc.
 PRODUCT_BUILD_PROP_OVERRIDES += \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo kylepro-userdebug
+add_lunch_combo aosp_kylepro-userdebug
