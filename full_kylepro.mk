009f54dc7c275226d04fce5fe1b0e8e11392d870
TheComputerGuy96 <vwestlife20@gmail.com>
2016-09-16 14:11:35 +0300

    Create full_kylepro.mk

---
+++ full_kylepro.mk
@@ -0,0 +1,15 @@
+# Get the long list of APNs
+PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit device configuration
+$(call inherit-product, $(LOCAL_PATH)/device_kylepro.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := kylepro
+PRODUCT_NAME := full_kylepro
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MODEL := GT-S7580
